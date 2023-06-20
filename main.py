c1db0b19d4c51d87bcbfbc4db8c6b40096ac395f
BorisToboltsov <b.toboltsov@vcore.su>
2023-06-20 16:53:05 +0300

    Init aiogram, sentry, dotenv

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+import sentry_sdk
+
+
+load_dotenv()
+
+sentry_sdk.init(os.getenv("API_TOKEN_SENTRY"))
+
+API_TOKEN_TELEGRAM = os.getenv("API_TOKEN_TELEGRAM")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN_TELEGRAM)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start", "help"])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
