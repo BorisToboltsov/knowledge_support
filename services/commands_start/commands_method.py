b2e19d65bcd7790277fbca4acbc6c4732b7cc17a
BorisToboltsov <b.toboltsov@vcore.su>
2023-09-07 09:47:45 +0300

    check user complete

--- services/commands_start/commands_method.py
+++ services/commands_start/commands_method.py
@@ -7,3 +7,5 @@ from services.profile.create_new_user import create_new_user
 async def start(message: Message):
     if await check_new_user(int(message.from_user.id)) is False:
         await create_new_user(message)
+    else:
+        pass
--- services/profile/check_new_user.py
+++ services/profile/check_new_user.py
@@ -1,5 +1,11 @@
+from sqlalchemy.orm.exc import NoResultFound
+
 from database.profile.crud.account import CrudAccount
 
 
 async def check_new_user(telegram_id) -> bool:
-    account = CrudAccount.get_account(4469980541)
+    try:
+        CrudAccount.get_account(telegram_id)
+        return True
+    except NoResultFound:
+        return False
