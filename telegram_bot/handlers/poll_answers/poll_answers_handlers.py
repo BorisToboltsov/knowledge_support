0bdf34839ac5ff8a7e6bc5d0328f22056a25dba1
BorisToboltsov <b.toboltsov@vcore.su>
2023-09-25 11:43:14 +0300

    rename poll_answers

--- telegram_bot/handlers/poll_answers/poll_answers_handlers.py
+++ telegram_bot/handlers/poll_answers/poll_answers_handlers.py
@@ -6,6 +6,6 @@ router_poll_answers = Router()
 
 
 @router_poll_answers.poll_answer()
-async def a(message):
+async def poll_answers(message):
     print("Прилет ответа!")
     profile_answers = get_profile_answers(message.user.id, message.poll_id)
