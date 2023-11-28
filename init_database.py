92869c9eff2b0ff37f4a740a4200552960d0c627
BorisToboltsov <b.toboltsov@vcore.su>
2023-11-28 14:49:04 +0300

    refactoring merge_json.py

--- init_database.py
+++ init_database.py
@@ -1,7 +1,7 @@
 from services.init_database.add_entity_language import InitDbEntityLanguage
 from services.init_database.add_filter_questions import InitDbFilterQuestions
 from services.init_database.add_language import InitDbSaveLanguage
-from services.init_database.migrate_database import save_question_answers
+from services.init_database.migrate_database import InitDbMigrateDatabase
 
 init_db_save_language = InitDbSaveLanguage()
 init_db_save_language.write_language()
@@ -12,4 +12,5 @@ init_db_entity_language.write_entity_language()
 init_db_filter_questions = InitDbFilterQuestions()
 init_db_filter_questions.write_filter_questions()
 
-save_question_answers()
+init_db_migrate_database = InitDbMigrateDatabase()
+init_db_migrate_database.write_migrate_database()
